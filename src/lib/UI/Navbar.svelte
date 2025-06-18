dd9b5b053aa79af400a20e0dd1c608534d744bac
Daniel Borowski <danielborowski05@gmail.com>
2025-06-18 19:22:07 +0200

    Dodano CV - 1.2.0

--- src/lib/UI/Navbar.svelte
+++ src/lib/UI/Navbar.svelte
@@ -47,7 +47,8 @@
 					<p>Kwalifikacje</p>
 				</a>
 				<a
-					href="/cv"
+					href="/Daniel_Borowski.pdf"
+					target="_blank"
 					data-sveltekit-reload
 					class=" text-neutral hover:text-gray-300 flex flex-row items-center gap-2 focus:outline-none focus:ring-2 focus:text-accent focus:ring-emerald-500 focus:ring-offset-2 focus:ring-offset-secondary rounded-lg p-1 transition-all duration-200"
 				>
@@ -108,7 +109,8 @@
 					<p>Kwalifikacje</p>
 				</a>
 				<a
-					href="/cv"
+					href="/Daniel_Borowski.pdf"
+					target="_blank"
 					on:click={() => (navbarVisible = false)}
 					data-sveltekit-reload
 					class=" text-neutral hover:text-gray-300 flex flex-row items-center gap-2 focus:outline-none focus:ring-2 focus:text-accent focus:ring-emerald-500 focus:ring-offset-2 focus:ring-offset-secondary rounded-lg p-1 transition-all duration-200"
